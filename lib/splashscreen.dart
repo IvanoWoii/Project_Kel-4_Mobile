d7c7d7c7e7775fe7aa1c45f6951fb04a671956e9
IvanoWoii <rezaivano123@gmail.com>
2022-11-09 09:24:18 +0700

    update login , register, splashscreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'login.dart';
 
 class SplahScreen extends StatefulWidget {
@@ -21,35 +22,57 @@ class _SplahScreenState extends State<SplahScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: new BoxDecoration(
-          gradient: new LinearGradient(
-            begin: FractionalOffset.topLeft,
-            end: FractionalOffset.bottomRight,
-            colors: [Color.fromARGB(250, 175, 1, 113), Colors.purpleAccent],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "img/logo no bg.png",
-              width: 300,
-              height: 300,
-            ),
-            Text(
-              "Tempat Dimana Print Tanpa Antri",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  fontFamily: "Monsterart"),
-            ),
-          ],
-        ),
+    return EasySplashScreen(
+      gradientBackground: LinearGradient(
+        begin: FractionalOffset.topLeft,
+        end: FractionalOffset.bottomRight,
+        colors: [Color.fromARGB(250, 175, 1, 113), Colors.purpleAccent],
       ),
+      logo: Image.asset("img/logo no bg.png"),
+      logoWidth: 150.0,
+      title: Text(
+        "APP PRON",
+        style: TextStyle(
+            fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+      showLoader: true,
+      loadingText: Text(
+        "Loading...",
+        style: TextStyle(color: Colors.white),
+      ),
+      navigator: login(),
+      durationInSeconds: 10,
+      loaderColor: Colors.white,
     );
+    // return Scaffold(
+    //   body: Container(
+    //     width: MediaQuery.of(context).size.width,
+    //     decoration: new BoxDecoration(
+    //       gradient: new LinearGradient(
+    //         begin: FractionalOffset.topLeft,
+    //         end: FractionalOffset.bottomRight,
+    //         colors: [Color.fromARGB(250, 175, 1, 113), Colors.purpleAccent],
+    //       ),
+    //     ),
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         Image.asset(
+    //           "img/logo no bg.png",
+    //           width: 300,
+    //           height: 300,
+    //         ),
+    //         Text(
+    //           "Tempat Dimana Print Tanpa Antri",
+    //           style: TextStyle(
+    //               color: Colors.white,
+    //               fontWeight: FontWeight.bold,
+    //               fontSize: 20,
+    //               fontFamily: "Monsterart"),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
