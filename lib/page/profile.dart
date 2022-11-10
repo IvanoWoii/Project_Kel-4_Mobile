acb982ea865fe49b41f14cd20aa51523b75583ba
DimasPratama03 <112364293+DimasPratama03@users.noreply.github.com>
2022-11-10 12:59:36 +0700

    Update profile.dart

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -8,102 +8,178 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(250, 175, 1, 113),
-        centerTitle: true,
-        title: const Text(
-          'Profile',
-          style:
-              TextStyle(fontSize: 17, color: Colors.white, letterSpacing: 0.53),
-        ),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(30),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(249, 131, 69, 137),
+          centerTitle: true,
+          title: const Text(
+            'Profile',
+            style: TextStyle(
+                fontSize: 17, color: Colors.white, letterSpacing: 0.53),
           ),
-        ),
-        leading: InkWell(
-          onTap: () {},
-          child: const Icon(
-            Icons.arrow_back_rounded,
-            color: Colors.white,
-          ),
-        ),
-        actions: [
-          InkWell(
-            onTap: () {},
-            child: const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Icon(
-                Icons.settings,
-                size: 20,
-              ),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(30),
             ),
           ),
-        ],
-        bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(110.0),
-            child: Container(
-              padding: const EdgeInsets.only(left: 30, bottom: 20),
-              child: Row(
-                children: [
-                  Stack(
-                    children: [
-                      const CircleAvatar(
-                        radius: 32,
-                        backgroundColor: Colors.white,
-                        child: Icon(Icons.person_outline_rounded),
-                      ),
-                      Container(
-                        height: 30,
-                        width: 30,
-                        decoration: const BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: const Icon(
-                          Icons.camera_alt_outlined,
-                          color: Colors.deepPurple,
-                          size: 20,
-                        ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(left: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'user123456',
-                          style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white),
-                        ),
-                        Text(
-                          'user123456@gmail.com',
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '+62 812 3456 7890',
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.white,
-                          ),
+          leading: InkWell(
+            onTap: () {},
+            child: const Icon(
+              Icons.arrow_back_rounded,
+              color: Colors.white,
+            ),
+          ),
+          actions: [
+            InkWell(
+              onTap: () {},
+              child: const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Icon(
+                  Icons.settings,
+                  size: 20,
+                ),
+              ),
+            ),
+          ],
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(110.0),
+              child: Container(
+                padding: const EdgeInsets.only(left: 30, bottom: 20),
+                child: Row(
+                  children: [
+                    Stack(
+                      children: [
+                        const CircleAvatar(
+                          radius: 32,
+                          backgroundColor: Colors.white,
+                          child: Icon(Icons.person_outline_rounded),
                         ),
                       ],
                     ),
-                  )
-                ],
+                    Container(
+                      margin: const EdgeInsets.only(left: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'user123456',
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            'user123456@gmail.com',
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            '+62 812 3456 7890',
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            height: size.height,
+            width: size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: size.height * .7,
+                  width: size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      ProfileWidget(
+                        icon: Icons.person,
+                        title: 'Profile Settings',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.settings,
+                        title: 'Settings',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.notifications,
+                        title: 'Notifications',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.chat,
+                        title: 'FAQs',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.share,
+                        title: 'Share',
+                      ),
+                      ProfileWidget(
+                        icon: Icons.logout,
+                        title: 'Log Out',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class ProfileWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  const ProfileWidget({
+    Key? key,
+    required this.icon,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                icon,
+                color: Colors.black.withOpacity(.5),
+                size: 24,
               ),
-            )),
-      ),
-      body: Center(
-        child: Text('Profile Screen', style: TextStyle(fontSize: 32)),
+              const SizedBox(
+                width: 16,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.black.withOpacity(.4),
+            size: 16,
+          )
+        ],
       ),
     );
   }
